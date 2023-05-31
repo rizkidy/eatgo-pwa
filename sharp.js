965ea8cd4153aa92c77450ef9e6bfa4b91ec81a9
rizkidy <rizkidy99@gmail.com>
2023-05-31 17:50:51 +0700

    fix image optimazion

--- sharp.js
+++ sharp.js
@@ -3,8 +3,8 @@ const sharp = require('sharp');
 const fs = require('fs');
 const path = require('path');
 
-const target = path.resolve(__dirname, 'src/public/images');
-const destination = path.resolve(__dirname, 'src/public/images');
+const target = path.resolve(__dirname, 'src/public/images/heros');
+const destination = path.resolve(__dirname, 'src/public/images/heros');
 
 if (!fs.existsSync(destination)) {
   fs.mkdirSync(destination);
